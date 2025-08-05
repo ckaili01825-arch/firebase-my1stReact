93c68d0e81db58aa5838d3eb6d4eba75112225bf
ckaili01825-arch <ckaili01825@gmail.com>
2025-08-06 00:59:53 +0800

    Initial commit: change firebase api

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -7,15 +7,18 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAAcylCn3V8UsC0PJdXrIAFAVgSKkny0fQ",
-  authDomain: "project01-my1streact.firebaseapp.com",
-  databaseURL: "https://project01-my1streact-default-rtdb.firebaseio.com",
-  projectId: "project01-my1streact",
-  storageBucket: "project01-my1streact.firebasestorage.app",
-  messagingSenderId: "318217172154",
-  appId: "1:318217172154:web:e6d73aa97282ad475bbe83",
-  measurementId: "G-FLDXVSGF80"
+  apiKey: "AIzaSyDNPkc5n_lDtNieEmS6WMnF3b3F27jQYbk",
+  authDomain: "ckaili-portfolio-website.firebaseapp.com",
+  projectId: "ckaili-portfolio-website",
+  storageBucket: "ckaili-portfolio-website.firebasestorage.app",
+  messagingSenderId: "509065369817",
+  appId: "1:509065369817:web:95820720375fa88b2a86c8",
+  measurementId: "G-RJFB0HT975"
 };
 
 // Initialize Firebase
